046fd3a854a6f1196b62e5bc67cea2739812f12f
Sasha1212123 <sasha.ivonin228@gmail.com>
2025-12-11 13:59:53 +0300

    Update models.py

--- databases/work_with_database/phones/models.py
+++ databases/work_with_database/phones/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
